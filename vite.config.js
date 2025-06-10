a8206a6bc99aac785163812343e5d62657abfd63
brucechrist <brucechristin.business@gmail.com>
2025-06-10 20:08:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',        // ensures relative paths for Electron
   plugins: [react()],
 });
