8e49925456efbe090db43dbe5fdeb0d2d0f083e2
brucechrist <brucechristin.business@gmail.com>
2025-06-13 12:04:35 +0200

    Make stats quadrant editable inline and larger

--- src/StatsQuadrant.jsx
+++ src/StatsQuadrant.jsx
@@ -3,22 +3,35 @@ import './stats-quadrant.css';
 
 export default function StatsQuadrant({ initialStats = [5, 5, 5, 5] }) {
   const [stats, setStats] = useState(initialStats);
+  const [editing, setEditing] = useState(null);
 
-  const updateStat = (index) => {
-    const newVal = prompt('Enter new value', stats[index]);
-    if (newVal !== null && newVal !== '') {
-      const updated = [...stats];
-      updated[index] = newVal;
-      setStats(updated);
-    }
+  const handleChange = (index, value) => {
+    const updated = [...stats];
+    updated[index] = value;
+    setStats(updated);
   };
 
   return (
     <div className="stats-quadrant">
-      <div className="quadrant top-left" onClick={() => updateStat(0)}>{stats[0]}</div>
-      <div className="quadrant top-right" onClick={() => updateStat(1)}>{stats[1]}</div>
-      <div className="quadrant bottom-left" onClick={() => updateStat(2)}>{stats[2]}</div>
-      <div className="quadrant bottom-right" onClick={() => updateStat(3)}>{stats[3]}</div>
+      {stats.map((stat, i) => (
+        <div
+          key={i}
+          className={`quadrant ${['top-left', 'top-right', 'bottom-left', 'bottom-right'][i]}`}
+          onClick={() => setEditing(i)}
+        >
+          {editing === i ? (
+            <input
+              type="number"
+              autoFocus
+              value={stat}
+              onChange={(e) => handleChange(i, e.target.value)}
+              onBlur={() => setEditing(null)}
+            />
+          ) : (
+            <span>{stat}</span>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
