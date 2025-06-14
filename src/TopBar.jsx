8481c5a2370e48407ca7789bd495bdd93525e449
brucechrist <brucechristin.business@gmail.com>
2025-06-14 18:22:11 +0200

    Load official Riot top bar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './topbar.css';
+import { useEffect } from 'react';
 
 export default function TopBar() {
-  return (
-    <header className="topbar">
-      <div className="logo">LoL</div>
-      <nav className="nav-links">
-        <a href="#">Actualités</a>
-        <a href="#">Univers</a>
-        <a href="#">Esports</a>
-        <a href="#">Plus de jeux</a>
-      </nav>
-    </header>
-  );
+  useEffect(() => {
+    const id = 'riotbar-script';
+    if (!document.getElementById(id)) {
+      const script = document.createElement('script');
+      script.id = id;
+      script.src =
+        'https://cdn.rgpub.io/public/live/riotbar/latest/lol.fr-fr.js';
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return <div id="riotbar-header" />;
 }
