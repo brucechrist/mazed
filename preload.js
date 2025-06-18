bf7305e69bd425f6f98020068fa1c3bd5684577c
brucechrist <brucechristin.business@gmail.com>
2025-06-18 23:28:31 +0200

    feat: add disconnect option

--- preload.js
+++ preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   onGoHome: (callback) => ipcRenderer.on('go-home', callback),
+  onDisconnect: (callback) => ipcRenderer.on('disconnect', callback),
 });
