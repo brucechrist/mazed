dc9bdeafd970d702978e87aa4659c3a4bd6d5700
brucechrist <brucechristin.business@gmail.com>
2025-06-23 19:08:09 +0200

    Document username login and bump version

--- profiles.sql
+++ profiles.sql
@@ -1,8 +1,22 @@
 create table if not exists profiles (
   id uuid references auth.users(id) primary key,
+  email text not null,
   username text unique not null,
   avatar_url text,
   resources int default 0,
   streaks int default 0,
   stats jsonb default '[5,5,5,5]'
 );
+
+-- Upgrade existing installations
+alter table profiles
+  add column if not exists email text;
+
+update profiles
+set email = auth.users.email
+from auth.users
+where profiles.email is null
+  and profiles.id = auth.users.id;
+
+alter table profiles
+  alter column email set not null;
