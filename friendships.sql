f770622dda791e195129a8310b50092e7a658201
brucechrist <brucechristin.business@gmail.com>
2025-08-27 17:57:04 +0200

    Delete friendships.sql

--- friendships.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists friendships (
-  user_id uuid references auth.users(id),
-  friend_id uuid references auth.users(id),
-  status text check (status in ('pending','accepted')) default 'pending',
-  primary key (user_id, friend_id)
-);
