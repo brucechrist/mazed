9624880917e764195125200e4bccf33570550cd3
brucechrist <brucechristin.business@gmail.com>
2025-08-27 18:05:06 +0200

    Delete VersionLabel.jsx

--- VersionLabel.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { APP_VERSION } from './version.js';
-import './version-label.css';
-
-export default function VersionLabel() {
-  return <div className="version-label">v{APP_VERSION}</div>;
-}
