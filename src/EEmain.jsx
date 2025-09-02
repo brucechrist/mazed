5b9466d61d233fcd6581f56e4c42b1e6252fe4fe
brucechrist <brucechristin.business@gmail.com>
2025-09-02 19:00:52 +0200

    feat: register multiple apps for docking

--- src/EEmain.jsx
+++ src/EEmain.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import QuadrantPage from './App.jsx';
 
-export default function EEmain({ menuBg, onChangeMenuBg }) {
+export default function EEmain({ menuBg, onChangeMenuBg, onOpenDock }) {
   return (
     <QuadrantPage
       initialTab="Friends"
       menuBg={menuBg}
       onChangeMenuBg={onChangeMenuBg}
+      onOpenDock={onOpenDock}
     />
   );
 }
