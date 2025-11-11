475ec017b72a0c5087d52f4bce3cab1ec1d7466b
brucechrist <brucechristin.business@gmail.com>
2025-11-11 21:59:33 +0100

    Show video thumbnails without hover

--- src/Library.jsx
+++ src/Library.jsx
@@ -1268,6 +1268,16 @@ export default function Library({ onBack }) {
           metadataNeedsUpdate = true;
         }
         const enrichedBase = { ...base, mimeType: resolvedMime };
+        if (resolvedMime.startsWith('video/') && !enrichedBase.thumbnail) {
+          const promise = generateVideoThumbnail(dataUrl).then((thumb) => {
+            if (!thumb) {
+              return false;
+            }
+            enrichedBase.thumbnail = thumb;
+            return true;
+          });
+          thumbnailPromises.push(promise);
+        }
 
         loaded.push({ base: enrichedBase, dataUrl, stored });
       }
@@ -2410,7 +2420,9 @@ export default function Library({ onBack }) {
           }
         }}
       >
-        {snd.thumbnail ? (
+        {isVideo ? (
+          <VideoPreview src={snd.dataUrl} poster={snd.thumbnail} title={snd.title} />
+        ) : snd.thumbnail ? (
           <img src={snd.thumbnail} alt={snd.title} draggable={false} />
         ) : (
           <div className="sound-placeholder">{placeholderIcon}</div>
@@ -2425,13 +2437,7 @@ export default function Library({ onBack }) {
             )}
             {snd.title}
           </h3>
-          {isVideo ? (
-            <VideoPreview
-              src={snd.dataUrl}
-              poster={snd.thumbnail}
-              title={snd.title}
-            />
-          ) : (
+          {isVideo ? null : (
             <audio
               controls
               src={snd.dataUrl}
